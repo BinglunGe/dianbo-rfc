42a2cdd2b3f5d75c0b3078ac4bd595992218c4f4
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-28 18:09:10 +0800

    Add util downloadRFC.js

---
+++ bin/downloadRFC.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { program } = require('commander');
+const process = require('process');
+const fse = require('fs-extra');
+const path = require('path');
+const http = require('http');
+
+program
+    .requiredOption('-i, --index <int>', 'RFC index')
+    .requiredOption('-d, --dir <path>', 'Output directory')
+    .parse(process.argv);
+
+const args = {}
+args.index = program.index;
+args.dir = program.dir;
+args.url = `http://www.rfc-editor.org/rfc/rfc${args.index}.txt`;
+args.file = path.join(args.dir, `rfc${args.index}.txt`);
+
+(async () => {
+    await fse.ensureDir(args.dir);
+    const data = await httpGet(args.url);
+    await fse.writeFile(args.file, data);
+    console.log('Done');
+})().catch(e => console.log(e));
+
+
+function httpGet(url) {
+    return new Promise((resolve, reject) => {
+        let data = '';
+        http.get(url, (response) => {
+            response.on('data', (chunk) => { data += chunk; });
+            response.on('end', () => { resolve(data); });
+        }).on('error', reject);
+    });
+};
