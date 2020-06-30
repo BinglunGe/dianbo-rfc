565d6a359bdfbc29014b4931c21eb2328932c663
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-30 17:52:11 +0800

    downloadRFC.js use https instead of http

--- bin/downloadRFC.js
+++ bin/downloadRFC.js
@@ -6,7 +6,7 @@ const { program } = require('commander');
 const process = require('process');
 const fse = require('fs-extra');
 const path = require('path');
-const http = require('http');
+const https = require('https');
 
 program
     .requiredOption('-i, --index <int>', 'RFC index')
@@ -16,21 +16,21 @@ program
 const args = {}
 args.index = program.index;
 args.dir = program.dir;
-args.url = `http://www.rfc-editor.org/rfc/rfc${args.index}.txt`;
+args.url = `https://www.rfc-editor.org/rfc/rfc${args.index}.txt`;
 args.file = path.join(args.dir, `rfc${args.index}.txt`);
 
 (async () => {
     await fse.ensureDir(args.dir);
-    const data = await httpGet(args.url);
+    const data = await httpsGet(args.url);
     await fse.writeFile(args.file, data);
     console.log('Done');
 })().catch(e => console.log(e));
 
 
-function httpGet(url) {
+function httpsGet(url) {
     return new Promise((resolve, reject) => {
         let data = '';
-        http.get(url, (response) => {
+        https.get(url, (response) => {
             response.on('data', (chunk) => { data += chunk; });
             response.on('end', () => { resolve(data); });
         }).on('error', reject);
