379076c806769da3f0a74d7743307becc12eed81
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-30 19:56:49 +0800

    Consider fatChar in maxLineWidth.js

--- bin/maxLineWidth.js
+++ bin/maxLineWidth.js
@@ -6,6 +6,8 @@ const { program } = require('commander');
 const process = require('process');
 const fse = require('fs-extra');
 
+const reFatChar = /[^\x00-\x7F“”]/g;
+
 program
     .requiredOption('-f, --file <path>', 'Input file')
     .parse(process.argv);
@@ -13,7 +15,12 @@ program
 (async () => {
     const text = '' + await fse.readFile(program.file);
     const lines = text.split('\n');
-    const maxLineWidth = lines.reduce((len, item) => len > item.length ? len : item.length, 0);
+    const maxLineWidth = lines.reduce((maxWidth, item) => {
+        const fatCharList = item.match(reFatChar);
+        const fatCharCnt = fatCharList ? fatCharList.length : 0;
+        const width = item.length + fatCharCnt;
+        return maxWidth > width ? maxWidth : width;
+    }, 0);
 
     process.stdout.write(`${maxLineWidth}\n`);
 })().catch(e => console.log(e));
