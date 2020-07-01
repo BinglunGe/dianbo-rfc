64660c4b5a26b558ab24ecb7d870bc5adf422a78
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-01 21:44:15 +0800

    Update rules in zhMarkTrans.js

--- bin/zhMarkTrans.js
+++ bin/zhMarkTrans.js
@@ -42,7 +42,7 @@ args.outFile = program.output || args.dftFile;
         { // 中文括号
             text = text.replace(/^([ \t]*)（/mg, '$1('); // 1. 行首的中文左括号
             text = errPat(text, /（[ \t]*$/mg, errSig); // 2. 行末的中文左括号
-            text = text.replace(/([（、(])[ \t]*（/g, '$1('); // 3. 其它符号后跟中文左括号
+            text = text.replace(/([。（、(])[ \t]*（/g, '$1('); // 3. 其它符号后跟中文左括号
             text = text.replace(/[ \t]*（/g, ' ('); // 4. 剩余的中文左括号
 
             text = errPat(text, /^[ \t]*）/mg, errSig); // 1. 行首的中文右括号
@@ -54,7 +54,7 @@ args.outFile = program.output || args.dftFile;
         { //中文双引号
             text = text.replace(/^([ \t]*)“/mg, '$1"'); // 1. 行首的中文左双引号
             text = errPat(text, /“[ \t]*$/mg, errSig); // 2. 行末的中文左双引号
-            text = text.replace(/([（、(])[ \t]*“/g, '$1"'); // 3. 其它符号后跟中文左双引号
+            text = text.replace(/([。（、(])[ \t]*“/g, '$1"'); // 3. 其它符号后跟中文左双引号
             text = text.replace(/[ \t]*“/g, ' "'); // 4. 剩余的中文左双引号
 
             text = errPat(text, /^[ \t]*”/mg, errSig); // 1. 行首的中文右双引号
@@ -65,7 +65,7 @@ args.outFile = program.output || args.dftFile;
         { //中文单引号
             text = text.replace(/^([ \t]*)‘/mg, "$1'"); // 1. 行首的中文左单引号
             text = errPat(text, /‘[ \t]*$/mg, errSig); // 2. 行末的中文左单引号
-            text = text.replace(/([（、(])[ \t]*‘/g, "$1'"); // 3. 其它符号后跟中文左单引号
+            text = text.replace(/([。（、(])[ \t]*‘/g, "$1'"); // 3. 其它符号后跟中文左单引号
             text = text.replace(/[ \t]*‘/g, " '"); // 4. 剩余的中文左单引号
 
             text = errPat(text, /^[ \t]*’/mg, errSig); // 1. 行首的中文右单引号
