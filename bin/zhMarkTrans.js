b205ef4bf786e0ac340c10802048735be180da8d
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-09 01:26:56 +0800

    Update zhMarkTrans.js due to RFC 2119

--- bin/zhMarkTrans.js
+++ bin/zhMarkTrans.js
@@ -39,49 +39,49 @@ args.outFile = program.output || args.dftFile;
         text = text.replace(/；[ \t]*/g, '; '); // 3. 剩余的中文分号
     }
     { // 中文圆括号、中文方括号、中文双引号、中文单引号
+        { // 中文方括号
+            // text = text.replace(/^([ \t]*)【/mg, '$1['); // 1. 行首的中文左方括号
+            text = errPat(text, /【[ \t]*$/mg, errSig); // 2. 行末的中文左方括号
+            // text = text.replace(/([。（、(])[ \t]*【/g, '$1['); // 3. 其它符号后跟中文左方括号
+            // text = text.replace(/[ \t]*【/g, ' ['); // 4. 剩余的中文左方括号
+
+            text = errPat(text, /^[ \t]*】/mg, errSig); // 1. 行首的中文右方括号
+            // text = text.replace(/】[ \t]*$/mg, ']'); // 2. 行末的中文右方括号
+            // text = text.replace(/】[ \t]*([。，；：）、.,;:)])/g, ']$1'); // 3. 中文右方括号后跟其它符号
+            // text = text.replace(/】[ \t]*/g, '] '); // 4. 剩余的中文右方括号
+        }
         { // 中文圆括号
             text = text.replace(/^([ \t]*)（/mg, '$1('); // 1. 行首的中文左圆括号
             text = errPat(text, /（[ \t]*$/mg, errSig); // 2. 行末的中文左圆括号
-            text = text.replace(/([。（、(])[ \t]*（/g, '$1('); // 3. 其它符号后跟中文左圆括号
+            text = text.replace(/([】。（、(])[ \t]*（/g, '$1('); // 3. 其它符号后跟中文左圆括号
             text = text.replace(/[ \t]*（/g, ' ('); // 4. 剩余的中文左圆括号
 
             text = errPat(text, /^[ \t]*）/mg, errSig); // 1. 行首的中文右圆括号
             text = text.replace(/^([ \t]*[a-b])）/mg, '$1)'); // 2. 用于小标题的中文右圆括号
             text = text.replace(/）[ \t]*$/mg, ')'); // 3. 行末的中文右圆括号
-            text = text.replace(/）[ \t]*([。，；：）、.,;:)])/g, ')$1'); // 4. 中文右圆括号后跟其它符号
+            text = text.replace(/）[ \t]*([【。，；：）、.,;:)])/g, ')$1'); // 4. 中文右圆括号后跟其它符号
             text = text.replace(/）[ \t]*/g, ') '); // 5. 剩余的中文右圆括号
-        }
-        { // 中文方括号
-            text = text.replace(/^([ \t]*)【/mg, '$1['); // 1. 行首的中文左方括号
-            text = errPat(text, /【[ \t]*$/mg, errSig); // 2. 行末的中文左方括号
-            text = text.replace(/([。（、(])[ \t]*【/g, '$1['); // 3. 其它符号后跟中文左方括号
-            text = text.replace(/[ \t]*【/g, ' ['); // 4. 剩余的中文左方括号
-
-            text = errPat(text, /^[ \t]*】/mg, errSig); // 1. 行首的中文右方括号
-            text = text.replace(/】[ \t]*$/mg, ']'); // 2. 行末的中文右方括号
-            text = text.replace(/】[ \t]*([。，；：）、.,;:)])/g, ']$1'); // 3. 中文右方括号后跟其它符号
-            text = text.replace(/】[ \t]*/g, '] '); // 4. 剩余的中文右方括号
         }
         { //中文双引号
             text = text.replace(/^([ \t]*)“/mg, '$1"'); // 1. 行首的中文左双引号
             text = errPat(text, /“[ \t]*$/mg, errSig); // 2. 行末的中文左双引号
-            text = text.replace(/([。（、(])[ \t]*“/g, '$1"'); // 3. 其它符号后跟中文左双引号
+            text = text.replace(/([】。（、(])[ \t]*“/g, '$1"'); // 3. 其它符号后跟中文左双引号
             text = text.replace(/[ \t]*“/g, ' "'); // 4. 剩余的中文左双引号
 
             text = errPat(text, /^[ \t]*”/mg, errSig); // 1. 行首的中文右双引号
             text = text.replace(/”[ \t]*$/mg, '"'); // 2. 行末的中文右双引号
-            text = text.replace(/”[ \t]*([。，；：）、.,;:)])/g, '"$1'); // 3. 中文右双引号后跟其它符号
+            text = text.replace(/”[ \t]*([【。，；：）、.,;:)])/g, '"$1'); // 3. 中文右双引号后跟其它符号
             text = text.replace(/”[ \t]*/g, '" '); // 4. 剩余的中文右双引号
         }
         { //中文单引号
             text = text.replace(/^([ \t]*)‘/mg, "$1'"); // 1. 行首的中文左单引号
             text = errPat(text, /‘[ \t]*$/mg, errSig); // 2. 行末的中文左单引号
-            text = text.replace(/([。（、(])[ \t]*‘/g, "$1'"); // 3. 其它符号后跟中文左单引号
+            text = text.replace(/([】。（、(])[ \t]*‘/g, "$1'"); // 3. 其它符号后跟中文左单引号
             text = text.replace(/[ \t]*‘/g, " '"); // 4. 剩余的中文左单引号
 
             text = errPat(text, /^[ \t]*’/mg, errSig); // 1. 行首的中文右单引号
             text = text.replace(/’[ \t]*$/mg, "'"); // 2. 行末的中文右单引号
-            text = text.replace(/’[ \t]*([。，；：）、.,;:)])/g, "'$1"); // 3. 中文右单引号后跟其它符号
+            text = text.replace(/’[ \t]*([【。，；：）、.,;:)])/g, "'$1"); // 3. 中文右单引号后跟其它符号
             text = text.replace(/’[ \t]*/g, "' "); // 4. 剩余的中文右单引号
         }
     }
